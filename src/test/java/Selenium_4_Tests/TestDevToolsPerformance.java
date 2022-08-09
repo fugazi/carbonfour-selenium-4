341b68d7007f8280ce92944b1eb138e44908406d
Douglas Fugazi <itmteleco@gmail.com>
2022-08-08 21:43:23 -0500

    Update TestDevToolsPerformance
    Updating with Performance Metrics objects to test.

--- src/test/java/Selenium_4_Tests/TestDevToolsPerformance.java
+++ src/test/java/Selenium_4_Tests/TestDevToolsPerformance.java
@@ -3,8 +3,20 @@ package Selenium_4_Tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v103.network.Network;
+import org.openqa.selenium.devtools.v103.performance.Performance;
+import org.openqa.selenium.devtools.v103.performance.model.Metric;
 import org.openqa.selenium.edge.EdgeDriver;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 public class TestDevToolsPerformance {
 
@@ -30,10 +42,64 @@ public class TestDevToolsPerformance {
     }
 
     /**
-     * Test the DevTools performance of the Edge browser.
+     * Performance Emulation Conditions using Selenium 4.0.
+     * DevTools has a method to emulate Performance Metrics like load time, largest contentful rate, timeline event and layout shift.
+     * <a href="Link">https://chromedevtools.github.io/devtools-protocol/tot/PerformanceTimeline/</a>
+     * The website under test should go after the 'requestWillBeSent' method.
      */
     @Test
-    void testPerformance() {
+    @DisplayName("Capture HTTP Request Performance Metrics")
+    void testPerformanceCaptureHttpRequests() {
+        // Get The DevTools & Create A Session with the ChromeDriver.
+        DevTools devTools = driver.getDevTools();
+        devTools.createSession();
+        // Enables network tracking, requests events will be now delivered to the client
+        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+        // Add an event Listener for the 'requestWillBeSent' event.
+        devTools.addListener(Network.requestWillBeSent(), event -> {
+            System.out.println("Request URI: " + event.getRequest().getUrl() + "\n"
+                    + "Request URI + Assertions: " + event.getRequest().getUrl().contains("linkedin.com") + "\n"
+                    + "Request Type: " + event.getType() + "\n"
+                    + "Request Method: " + event.getRequest().getMethod() + "\n"
+                    + "Request Headers: " + event.getRequest().getHeaders() + "\n"
+                    + "Request Post Data: " + event.getRequest().getPostData() + "\n"
+                    + "Request Mixed Content Type: " + event.getRequest().getMixedContentType() + "\n"
+                    + "Request Referrer Policy: " + event.getRequest().getReferrerPolicy() + "\n"
+                    + "Request Is Signed Exchanged: " + event.getRequest().getUrlFragment().isPresent() + "\n"
+                    + "Request Document URL: " + event.getDocumentURL() + "\n"
+                    + "Request Initiator: " + event.getInitiator() + "\n"
+                    + "Request Timestamp: " + event.getTimestamp() + "\n");
+        });
+        // Active URL under test
+        driver.get("https://linkedin.com");
+        assertSoftly(softly -> softly.assertThat(driver.getTitle()).contains("LinkedIn"));
+        devTools.send(Network.disable());
+    }
 
+    @Test
+    @DisplayName("Performance Metrics objects")
+    void testPerformanceMetrics() {
+        // Get The DevTools & Create A Session with the ChromeDriver.
+        DevTools devTools = driver.getDevTools();
+        devTools.createSession();
+        // Enables performance tracking, requests events will be shown in the timeline panel.
+        devTools.send(Performance.enable(Optional.empty()));
+        // Active URL under test
+        driver.get("https://linkedin.com");
+        // Return a list of Performance Metrics objects then stream to get all of the names of the metrics captured
+        List<Metric> performanceMetrics = devTools.send(Performance.getMetrics());
+        List<String> metricNames = performanceMetrics.stream().map(Metric::getName).collect(Collectors.toList());
+        // Disables performance tracking
+        devTools.send(Performance.disable());
+        // Print the metrics captured
+        List<String> metricsAssert = Arrays.asList(
+                "Timestamp", "Documents", "Frames", "JSEventListeners",
+                "Nodes", "LayoutCount", "RecalcStyleCount", "RecalcStyleDuration", "LayoutDuration", "MediaKeySessions",
+                "Resources", "DomContentLoaded", "NavigationStart", "TaskDuration", "JSHeapUsedSize",
+                "JSHeapTotalSize", "ScriptDuration"
+        );
+        // Assert that the metrics captured are the expected metrics
+        metricsAssert.forEach(metric -> System.out.println("Metric: " + metric + "\n" + performanceMetrics.get(metricNames.indexOf(metric)).getValue()));
+        assertSoftly(softly -> softly.assertThat(metricNames).containsAll(metricsAssert));
     }
 }
