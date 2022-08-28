0abb5c87b0179fbb816c4c0287a0d7a7ca2356c9
Douglas Fugazi <itmteleco@gmail.com>
2022-08-28 17:58:13 -0500

    Updating Tests Practice
    Updating Tests Practice

--- src/test/java/Selenium_4_Tests_Practice/Pages/RegisterPage.java
+++ src/test/java/Selenium_4_Tests_Practice/Pages/RegisterPage.java
@@ -1,4 +1,74 @@
 package Selenium_4_Tests_Practice.Pages;
 
+import Selenium_4_Tests_Practice.Components.FormFieldComponent;
+import Selenium_4_Tests_Practice.Components.InputTextComponent;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
 public class RegisterPage {
+
+    private final WebElement element;
+
+    public RegisterPage(WebElement element) {
+        this.element = element;
+    }
+
+    private enum Using {
+        ERROR_MESSAGES(By.xpath("//div[@class='text-danger']"));
+
+        public final By selector;
+        Using(By selector) {
+            this.selector = selector;
+        }
+    }
+
+    /**
+     * Interface Form Fields.
+     */
+    public interface FormField<P> {
+        By getSelector();
+        FormFieldComponent getComponent(WebElement element, P page);
+    }
+
+    /**
+     * Enum containing the elements of Register Account Form
+     * Also including 'First Name and Last name' fields with the minimum and maximum characters for each component
+     */
+    public enum FirstAndLastName implements FormField<RegisterPage> {
+        FIRST_NAME_INPUT(By.xpath("//input[@id='input-firstname']"), (element, page) -> new InputTextComponent(element, 1, 32)),
+        LAST_NAME_INPUT(By.xpath("//input[@id='input-lastname']"), (element, page) -> new InputTextComponent(element, 1, 32));
+
+        final By selector;
+        final BiFunction<WebElement, WebDriver, FormFieldComponent> componentFactory;
+
+        FirstAndLastName(By selector, BiFunction<WebElement, WebDriver, FormFieldComponent> componentFactory) {
+            this.selector = selector;
+            this.componentFactory = componentFactory;
+        }
+
+        @Override
+        public By getSelector() {
+            return selector;
+        }
+
+        @Override
+        public FormFieldComponent getComponent(WebElement element, RegisterPage page) {
+            return componentFactory.apply(element, (WebDriver) page);
+        }
+    }
+
+    /**
+     * Gets the Error messages from the Register Account Form
+     *
+     * @return The list containing the error messages
+     */
+    public List<String> getFieldErrorMessages() {
+        return element.findElements(Using.ERROR_MESSAGES.selector).stream().map(WebElement::getText)
+                .collect(Collectors.toList());
+    }
 }
--- src/test/java/Selenium_4_Tests_Practice/RegisterTest.java
+++ src/test/java/Selenium_4_Tests_Practice/RegisterTest.java
@@ -1,4 +1,86 @@
 package Selenium_4_Tests_Practice;
 
+import Selenium_4_Tests_Practice.Components.FormFieldComponent;
+import Selenium_4_Tests_Practice.Pages.RegisterPage;
+import Selenium_4_Tests_Practice.Utilities.FormFieldUtility;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
 public class RegisterTest {
+
+    private static final int EXPECTED_ERRORS_TOTAL = 2;
+
+    public WebDriver driver;
+
+    /**
+     * Instantiate the RegisterPage class to the Webdriver object.
+     */
+    RegisterPage registerPage = new RegisterPage(driver);
+
+    /**
+     * Initialize the WebDriverManager and EdgeDriver.
+     * Go to the website under Test and maximize the browser window.
+     */
+    @BeforeEach
+    public void setupUrl() {
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://my-location.org");
+    }
+
+    /**
+     * Close the browser window.
+     */
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    /**
+     * Test to validate the Register Page.
+     */
+    @ParameterizedTest
+    @MethodSource("Source")
+    void validateFormFieldErrors(RegisterPage.FormField formField) {
+        FormFieldComponent formFieldComponent = registerPage.getFieldErrorMessages().get(formField);
+        FormFieldUtility formFieldUtility = FormFieldUtility.getInstance(formFieldComponent);
+        formFieldUtility.checkInputText();
+    }
+
+    private static Stream<RegisterPage.FormField formField> Source() {
+        return Stream.of(Arrays.stream(RegisterPage.FormField.values()))
+                .flatMap(Function.identity());
+    }
+
+    @Test
+    void testRegister() {
+        // validate error messages
+        List<String> errorMessages = registerPage.getFieldErrorMessages();
+        assertSoftly(softly -> {
+            softly.assertThat(errorMessages.size()).as("The field error message count is not as expected")
+                    .isEqualTo(EXPECTED_ERRORS_TOTAL);
+            for (String errorMessage : errorMessages) {
+                softly.assertThat(errorMessage.contains("Field is required") || errorMessage.contains("The validation is not valid"))
+                        .as("The field error message is not as expected. Actual message: " + errorMessage)
+                        .isTrue();
+            }
+        });
+
+    }
 }
+
