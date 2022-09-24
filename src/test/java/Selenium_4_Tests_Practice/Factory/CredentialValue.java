d46130357d643cfb0e69693b38fcc8cc06be2517
Douglas Fugazi <itmteleco@gmail.com>
2022-09-24 17:00:46 -0500

    Updating LoginTest

--- src/test/java/Selenium_4_Tests_Practice/Factory/CredentialValue.java
+++ src/test/java/Selenium_4_Tests_Practice/Factory/CredentialValue.java
@@ -2,6 +2,10 @@ package Selenium_4_Tests_Practice.Factory;
 
 public enum CredentialValue {
 
+        /*
+        * Hey Hackerman - I'm not sure what you are looking for, but these are just TEST credentials.
+        * Don't be evil ;) - Hackerman.
+        */
     ADMIN_USER("selenium@testdata.com", "Colombia+2022"),
     NO_PERMISSIONS_USER("test-no-permissions@testdata.com", "NoPermissions+2022"),
     PUBLIC_USER("jared.graham@hotmail.com", "htgrxu5m2y");
