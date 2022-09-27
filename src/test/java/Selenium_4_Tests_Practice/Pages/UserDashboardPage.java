5b92b6f60a25f7fc6ccc17e2506c5f1fdf557f08
Douglas Fugazi <itmteleco@gmail.com>
2022-09-26 21:56:05 -0500

    Update UserDashboardPage.java
    Updating UserDashboardPage.

--- src/test/java/Selenium_4_Tests_Practice/Pages/UserDashboardPage.java
+++ src/test/java/Selenium_4_Tests_Practice/Pages/UserDashboardPage.java
@@ -2,9 +2,11 @@ package Selenium_4_Tests_Practice.Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class UserDashboardPage {
 
+    private static final String VALUE_ATTRIBUTE = "value";
     private final WebDriver driver;
 
     /**
@@ -21,7 +23,8 @@ public class UserDashboardPage {
         USER_DASHBOARD_TABLE(By.xpath("//div[@id='content']")),
         ADDRESS_BOOK_LINK(By.xpath("//a[normalize-space()='Address Book']")),
         ADD_NEW_ADDRESS_BUTTON(By.xpath("//a[normalize-space()='New Address']")),
-        LOGOUT_LINK(By.xpath("//a[normalize-space()='Logout']"));
+        INPUT_FIRST_NAME(By.id("input-firstname")),
+        INPUT_LAST_NAME(By.id("input-lastname"));
 
         public final By selector;
 
@@ -56,4 +59,44 @@ public class UserDashboardPage {
      */
     public void clickNewAddressButton() {
         driver.findElement(Using.ADD_NEW_ADDRESS_BUTTON.selector).click(); }
+
+    /**
+     * Sets the First Name.
+     *
+     * @param firstName First Name in the form.
+     */
+    public void enterFirstName(String firstName) {
+        WebElement firstNameInput = driver.findElement(Using.INPUT_FIRST_NAME.selector);
+        firstNameInput.clear();
+        firstNameInput.sendKeys(firstName);
+    }
+
+    /**
+     * Gets the entered value of the First Name.
+     *
+     * @return First Name value.
+     */
+    public String getFirstName() {
+        return driver.findElement(Using.INPUT_FIRST_NAME.selector).getAttribute(VALUE_ATTRIBUTE);
+    }
+
+    /**
+     * Sets the Last Name.
+     *
+     * @param lastName Last Name in the form.
+     */
+    public void enterLastName(String lastName) {
+        WebElement lastNameInput = driver.findElement(Using.INPUT_LAST_NAME.selector);
+        lastNameInput.clear();
+        lastNameInput.sendKeys(lastName);
+    }
+
+    /**
+     * Gets the entered value of the Last Name.
+     *
+     * @return Last Name value.
+     */
+    public String getLastName() {
+        return driver.findElement(Using.INPUT_LAST_NAME.selector).getAttribute(VALUE_ATTRIBUTE);
+    }
 }
