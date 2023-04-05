2435ec493f0181cc62a219ff125930da3c302682
Douglas Fugazi <itmteleco@gmail.com>
2023-04-04 19:16:19 -0500

    Test using Scroll from ViewPort Selenium 4.0

--- src/test/java/Selenium_4_Tests/TestWheelScrolling.java
+++ src/test/java/Selenium_4_Tests/TestWheelScrolling.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.WheelInput;
 
 public class TestWheelScrolling {
 
@@ -43,5 +44,11 @@ public class TestWheelScrolling {
         actions.scrollByAmount(0, 500).perform();
         // Scroll up by 500 pixels
         actions.scrollByAmount(0, -500).perform();
+        // Scroll to an element
+        actions.scrollFromOrigin(WheelInput.ScrollOrigin.fromElement(
+                driver.findElement(By.xpath("//h3[normalize-space()='From The Blog']"))),0, 500).perform();
+        // Scroll from Viewport
+        actions.scrollFromOrigin(WheelInput.ScrollOrigin.fromViewport(),0, 500).perform();
+        actions.scrollFromOrigin(WheelInput.ScrollOrigin.fromViewport(35, 35),0, 500).perform();
     }
 }
