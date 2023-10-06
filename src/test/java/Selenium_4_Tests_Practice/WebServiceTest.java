31db3e8de063a0fdc64acf599ae0260c152b466a
Douglas Fugazi <itmteleco@gmail.com>
2023-10-06 15:27:16 -0500

    Updating WebServiceTest

--- src/test/java/Selenium_4_Tests_Practice/WebServiceTest.java
+++ src/test/java/Selenium_4_Tests_Practice/WebServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.io.IOException;
 
 import Selenium_4_Tests_Practice.Utilities.Environments.BaseDomain;
+import jdk.jfr.Description;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -15,20 +16,30 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-public class WebServiceTest {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * This class performs integration tests for the WebService.
+ * It performs authenticating, and making an HTTP POST request to the API,
+ * and expects a 201 (Created) response status code.
+ */
+public class WebServiceTest {
     private HttpClient httpClient;
-    private String secureToken;
+    private String tokenLogin;
 
     private static final String EMAIL = "user@login.com";
     private static final String PASSWORD = "password123";
     private static final String DOMAIN_ENV_VAR = "domain";
     private static final String DOMAIN_BY_DEFAULT = "https://webservice.com";
-    private static final String ENDPOINT = "/rest/user/login";
+    private static final String ENDPOINT = "/rest/service/test";
 
+    /**
+     * Initializes the HTTP client and performs user authentication to obtain the
+     * token login from Login WebService.
+     */
     @BeforeEach
     public void setUp() {
         httpClient = HttpClients.createDefault();
@@ -36,41 +47,46 @@ public class WebServiceTest {
     }
 
     /**
-     * WebService Login
+     * Performs user authentication from Login WebService by making an HTTP POST request and extracts
+     * the secure token from the response.
      */
 
     private void login() {
 
-        HttpPost authRequest = new HttpPost(DOMAIN_BY_DEFAULT + ENDPOINT);
+        HttpPost authRequest = new HttpPost("https://login.webservice.com/rest/user/");
         String authData = "email=" + EMAIL + "&password=" + PASSWORD;
         authRequest.setEntity(new StringEntity(authData, ContentType.APPLICATION_FORM_URLENCODED));
 
-        // Determine the environment based on the base URI
-        String baseUri = BaseDomain.getGlobalServiceURL(
-                System.getProperty(DOMAIN_ENV_VAR) != null ? System.getProperty(DOMAIN_ENV_VAR) : DOMAIN_BY_DEFAULT);
-        String apiBaseUrl = String.format("https://%s", baseUri) + ENDPOINT;
-
         try {
             HttpResponse response = httpClient.execute(authRequest);
-            assertEquals(200, response.getStatusLine().getStatusCode());
-            secureToken = EntityUtils.toString(response.getEntity());
+            // Parse the JSON response to extract the secureToken value
+            String jsonResponse = EntityUtils.toString(response.getEntity());
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(jsonResponse);
+            tokenLogin = jsonNode.get("secureToken").asText();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     /**
-     * WebService SignOut
+     * Performs a regression test to verify the functionality of the 'WebService' domain
+     * by logging in, authenticating, and making an HTTP POST request to the API.
+     * It expects the response to have a status code of 201 (Created).
      */
-    @Tag("Regression")
     @Test
-    void verifySignOut() {
-        assertNotNull(secureToken);
+    @Description("Regression Test")
+    void verifyWebService() {
+        assertNotNull(tokenLogin);
+        // Determine the environment based on the base URI
+        String baseUri = BaseDomain.getGlobalServiceURL(
+                System.getProperty(DOMAIN_ENV_VAR) != null ? System.getProperty(DOMAIN_ENV_VAR) : DOMAIN_BY_DEFAULT);
+        String apiBaseUrl = String.format("https://%s", baseUri) + ENDPOINT;
 
-        HttpPost request = new HttpPost("https://webservice.com/rest/user/logout");
+        HttpPost request = new HttpPost(apiBaseUrl);
         request.setHeader("Content-Type", "application/json");
         request.setHeader("Accept", "application/json;q=0.9;v=1");
-        request.setHeader("Identity-Token", secureToken);
+        request.setHeader("Secure-Token", tokenLogin);
 
         try {
             HttpResponse response = httpClient.execute(request);
