342fe68dc9d0ffa29e4b9a043268abdc6782b6bf
Douglas Fugazi <itmteleco@gmail.com>
2024-05-20 19:00:12 -0500

    devtools updated

--- src/test/java/Selenium_4_Tests/TestDevToolsConsoleLogs.java
+++ src/test/java/Selenium_4_Tests/TestDevToolsConsoleLogs.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v123.log.Log;
+import org.openqa.selenium.devtools.v125.log.Log;
 import org.openqa.selenium.edge.EdgeDriver;
 
 @Slf4j
